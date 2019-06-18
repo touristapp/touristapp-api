af3f3b94129c482a480fd4d33a5b02ba828aa64f
Steeve Randriamampianina <st2diio@protonmail.ch>
2019-06-18 16:09:44 +0200

    Enabling database connection

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 import iconv from 'iconv-lite';
 import encodings from 'iconv-lite/encodings';
 import app from './App';
-// import { db } from './database/initdb';
+import { db } from './database/initdb';
 
 iconv.encodings = encodings;
 require('dotenv').config();
 const port = process.env.PORT || 8080 ;
 
-// if (process.env.NODE_ENV) {
-//   db.sync({ force: false }); // true: drops all tables first
-// } else {
-//   throw new Error('CONFIG ERROR : Please specify your NODE_ENV in an env file')
-// }
+if (process.env.NODE_ENV) {
+  db.sync({ force: true }); // true: drops all tables first
+} else {
+  throw new Error('CONFIG ERROR : Please specify your NODE_ENV in an env file')
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
